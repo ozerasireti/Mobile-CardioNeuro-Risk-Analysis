edaadb975715951033ad313475332037e05c69b5
ozerasireti <ozerfatime73@gmail.com>
2026-02-16 16:25:52 +0300

    Add pulse analysis function to evaluate heart rate

--- pulse_demo.py
+++ pulse_demo.py
@@ -1,1 +1,14 @@
+import random
 
+def pulse_analysis():
+    bpm = random.randint(60,110)
+    print("Ölçülen Nabız:", bpm)
+
+    if bpm < 50:
+        print("Bradikardi Riski")
+    elif 60 <= bpm <= 100:
+        print("Normal")
+    else:
+        print("Taşikardi Riski")
+
+pulse_analysis()
